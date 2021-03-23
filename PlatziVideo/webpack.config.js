0c038331bd0021aef1d726d3d46631909c54d362
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-23 17:45:03 -0600

    Optimización del Build

--- PlatziVideo/webpack.config.js
+++ PlatziVideo/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 require('dotenv').config();
 
 const isDev = (process.env.ENV === 'development');
@@ -28,14 +29,6 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
       },
       {
         test: /\.(s*)css$/,
@@ -67,8 +60,17 @@ module.exports = {
     isDev ? new webpack.HotModuleReplacementPlugin() :
       () => { }
     ,
+    isDev ? () => {} :
+      new CompressionWebpackPlugin({
+        test: /\.js$|\.css$/,
+        filename: '[path][base].gz',
+      }),
     new MiniCssExtractPlugin({
       filename: 'assets/app.css'
     }),
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [ new TerserWebpackPlugin()]
+  }
 };
